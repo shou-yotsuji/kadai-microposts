8be7b7790bacfa774241a2e6b5478ae51be7a43f
shou.yotsuji <basketball0420100@gmail.com>
2020-05-28 03:11:57 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
 
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -10,11 +10,12 @@ class UsersController < ApplicationController
     @microposts = @user.microposts.order(id: :desc).page(params[:page])
     counts(@user)
   end
+
   def new
-     @user = User.new
+    @user = User.new
   end
 
-def create
+  def create
     @user = User.new(user_params)
 
     if @user.save
@@ -24,11 +25,23 @@ def create
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
-end
+  end
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
 
   private
 
-def user_params
+  def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
+  end
 end
